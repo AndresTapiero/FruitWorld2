eb44e3285f67d8b0334f5971d920ecd55817ab82
AndresTapiero <andrestapiero72@gmail.com>
2019-01-14 22:08:49 -0500

    Mejora de la recyclerview y algunos componentes terminar el adaptador

--- app/src/main/java/com/example/andrestapiero/lab3fruitworl2/adapters/MyAdapter.java
+++ app/src/main/java/com/example/andrestapiero/lab3fruitworl2/adapters/MyAdapter.java
@@ -1,11 +1,10 @@
-package com.example.andrestapiero.lab3fruitworl2;
+package com.example.andrestapiero.lab3fruitworl2.adapters;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -49,7 +48,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
        public ViewHolder(View itemView){
            super(itemView);
-            this.name = itemView.findViewById(R.id.textViewName);
+          //  this.name = itemView.findViewById(R.id.textViewName);
        }
 
    }
